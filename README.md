01acbb2eee764466467c200ad1740d644417ea9a
Mark Farver <mfarver@mindbent.org>
2013-08-27 22:22:17 -0400

    Added license header, and fixed overrun
    Fixed issue where last character would read columns from beyond end of
    message array.. now correctly wraps back to the beginning.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 GrrCON_2013_Badge
 =================
 
-Schematics, Board Designs and code for GrrCON 2013 Badges
+Schematics, Board Designs and code for GrrCON 2013 Badges.
+
+Schematic and Board layouts require Eagle 6.4.
+
+
--- Software/BasicSign/BasicSign.ino
+++ Software/BasicSign/BasicSign.ino
@@ -1,4 +1,29 @@
-/* Basic scrolling Signboard app for the GrrCON badge */
+/* Example scrolling signboard app for GrrCON 2013 badges */
+/* Author: Mark Farver                                    */
+/* Date:   26 Aug 2013                                    */
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Mark Farver
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 #include "font5x7.h"
 
@@ -18,7 +43,7 @@ void setup() {
 }
 
 //offset into the message, where screen draw should start
-unsigned int current_col = 12;  
+unsigned int current_col = 0;  
 
 //How many times should we paint the matrix before scrolling it
 const int SCROLL_REFRESH = 50;
@@ -30,14 +55,13 @@ int scroll_count = 0;
 void loop() {
   char letter = ' ';
   unsigned int offset = 0;
+  unsigned int print_col;
   unsigned int i = 0;
 
-  // TODO, seems like we have an off by one error that cuases a brief garbage
-  //   column at the end of scroll.  I think it is becuase we are not handling
-  //   wrap around correctly.  
   for (i=0; i < 8; ++i) {
-    letter = message[(current_col + i) / CHAR_WIDTH];
-    offset = (current_col + i) % CHAR_WIDTH;
+    print_col = (current_col + i) % MESSAGE_COLS;
+    letter = message[print_col / CHAR_WIDTH];
+    offset = print_col % CHAR_WIDTH;
     writeCol(i, font5x7[ (letter - FIRST_CHAR) * CHAR_WIDTH + offset]);
   }  
 
